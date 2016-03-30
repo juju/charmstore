ericsnowcurrently/juju-charmstore

Add Store.OpenResource().

internal/charmstore/resources.go
@@ -4,13 +4,17 @@
 package charmstore // import "gopkg.in/juju/charmstore.v5-unstable/internal/charmstore"
 
 import (
+	"io"
+
 	"gopkg.in/errgo.v1"
 	"gopkg.in/juju/charm.v6-unstable"
 	"gopkg.in/juju/charm.v6-unstable/resource"
+	"gopkg.in/juju/charmrepo.v2-unstable/csclient/params"
 	"gopkg.in/mgo.v2"
 	"gopkg.in/mgo.v2/bson"
 
 	"gopkg.in/juju/charmstore.v5-unstable/internal/mongodoc"
+	"gopkg.in/juju/charmstore.v5-unstable/internal/router"
 )
 
 var resourceNotFound = errgo.Newf("resource not found")
@@ -49,18 +53,43 @@ func (s Store) ListResources(entity *mongodoc.Entity) ([]resource.Resource, erro
 }
 
 func (s Store) latestResource(entity *mongodoc.Entity, resName string) (resource.Resource, error) {
-	revision, ok := entity.Resources[resName]
-	if !ok {
-		// TODO(ericsnow) Fail if the resource otherwise exists?
-		return resource.Resource{}, resourceNotFound
+	revision, err := s.latestResourceRevision(entity, resName)
+	if err != nil {
+		return resource.Resource{}, err
 	}
 	// TODO(ericsnow) We need to pass in a base ID...
-	return s.resource(entity.URL, resName, revision)
+	res, _, err := s.resource(entity.URL, resName, revision)
+	return res, err
 }
 
-func (s Store) resource(curl *charm.URL, resName string, revision int) (resource.Resource, error) {
-	var res resource.Resource
+func (s Store) latestResourceRevision(entity *mongodoc.Entity, resName string) (int, error) {
+	latest, ok := entity.Resources[resName]
+	if !ok {
+		// TODO(ericsnow) Fail if the resource otherwise exists?
+		return -1, resourceNotFound
+	}
+	return latest, nil
+}
 
+func (s Store) resource(curl *charm.URL, resName string, revision int) (res resource.Resource, blobname string, err error) {
+	var doc mongodoc.Resource
+	id := mongodoc.NewResourceID(curl, resName, revision)
+	err = s.DB.Resources().FindId(id).One(&doc)
+	if err == mgo.ErrNotFound {
+		// TODO(ericsnow) Fail because "latest" points to a missing resource?
+		err = resourceNotFound
+	}
+	if err != nil {
+		return resource.Resource{}, "", err
+	}
+	res, err = mongodoc.Doc2Resource(doc)
+	if err != nil {
+		return res, "", errgo.Notef(err, "failed to convert resource doc")
+	}
+	return res, doc.BlobName, nil
+}
+
+func (s Store) resourceDoc(curl *charm.URL, resName string, revision int) (mongodoc.Resource, error) {
 	var doc mongodoc.Resource
 	id := mongodoc.NewResourceID(curl, resName, revision)
 	err := s.DB.Resources().FindId(id).One(&doc)
@@ -68,19 +97,62 @@ func (s Store) resource(curl *charm.URL, resName string, revision int) (resource
 		// TODO(ericsnow) Fail because "latest" points to a missing resource?
 		err = resourceNotFound
 	}
-	if err != nil {
-		return res, err
-	}
-
-	res, err = mongodoc.Doc2Resource(doc)
-	if err != nil {
-		return res, errgo.Notef(err, "failed to convert resource doc")
-	}
-
-	return res, nil
+	return doc, err
 }
 
-func (s Store) insertResource(entity *mongodoc.Entity, res resource.Resource, newRevision int) error {
+// OpenResource returns the blob for the identified resource.
+func (s *Store) OpenResource(id *router.ResolvedURL, name string, revision int) (resource.Resource, io.ReadCloser, error) {
+	if revision < 0 {
+		entity, err := s.FindEntity(id, FieldSelector("resources"))
+		if err != nil {
+			return resource.Resource{}, nil, errgo.Mask(err, errgo.Is(params.ErrNotFound))
+		}
+		revision, err = s.latestResourceRevision(entity, name)
+		if err != nil {
+			return resource.Resource{}, nil, err
+		}
+	}
+	res, blobName, err := s.resource(&id.URL, name, revision)
+	if err != nil {
+		return resource.Resource{}, nil, err
+	}
+	r, err := s.openResource(res, blobName)
+	if err != nil {
+		return resource.Resource{}, nil, err
+	}
+	return res, r, nil
+}
+
+func (s *Store) openResource(res resource.Resource, blobName string) (io.ReadCloser, error) {
+	if blobName == "" {
+		return nil, errgo.Newf("missing resource storage location")
+	}
+	r, size, err := s.BlobStore.Open(blobName)
+	if err != nil {
+		return nil, errgo.Notef(err, "cannot open resource data for %s", res.Name)
+	}
+	if size != res.Size {
+		return nil, errgo.Newf("resource size mismatch")
+	}
+	// TODO(ericsnow) Verify that the hash matches?
+	return r, nil
+}
+
+func (s Store) addResource(entity *mongodoc.Entity, res resource.Resource, blob io.Reader, newRevision int) error {
+	blobName, err := s.storeResource(entity, res, blob)
+	if err := mongodoc.CheckCharmResource(entity, res); err != nil {
+		return err
+	}
+	if s.insertResource(entity, res, blobName, newRevision); err != nil {
+		if err := s.BlobStore.Remove(blobName); err != nil {
+			logger.Errorf("cannot remove blob %s after error: %v", blobName, err)
+		}
+		return err
+	}
+	return nil
+}
+
+func (s Store) insertResource(entity *mongodoc.Entity, res resource.Resource, blobName string, newRevision int) error {
 	res.Revision = newRevision
 	if err := mongodoc.CheckCharmResource(entity, res); err != nil {
 		return err
@@ -90,6 +162,7 @@ func (s Store) insertResource(entity *mongodoc.Entity, res resource.Resource, ne
 	if err != nil {
 		return err
 	}
+	doc.BlobName = blobName
 
 	err = s.DB.Resources().Insert(doc)
 	if err != nil && !mgo.IsDup(err) {
@@ -99,11 +172,36 @@ func (s Store) insertResource(entity *mongodoc.Entity, res resource.Resource, ne
 	return nil
 }
 
+func (s Store) storeResource(entity *mongodoc.Entity, res resource.Resource, blob io.Reader) (string, error) {
+	name := bson.NewObjectId().Hex()
+
+	// Calculate the SHA384 hash while uploading the blob in the blob store.
+	fpHash := resource.NewFingerprintHash()
+	blob = io.TeeReader(blob, fpHash)
+
+	// Upload the actual blob, and make sure that it is removed
+	// if we fail later.
+	err := s.BlobStore.PutUnchallenged(blob, name, res.Size, res.Fingerprint.String())
+	if err != nil {
+		return "", errgo.Notef(err, "cannot put archive blob")
+	}
+
+	fp := fpHash.Fingerprint()
+	if fp.String() != res.Fingerprint.String() {
+		if err := s.BlobStore.Remove(name); err != nil {
+			logger.Errorf("cannot remove blob %s after error: %v", name, err)
+		}
+		return "", errgo.Newf("resource hash mismatch")
+	}
+
+	return name, nil
+}
+
 // TODO(ericsnow) We will need Store.nextResourceRevision()...
 
 func (s Store) setResource(entity *mongodoc.Entity, resName string, revision int) error {
 	// TODO(ericsnow) We need to pass in a base ID...
-	res, err := s.resource(entity.URL, resName, revision)
+	res, _, err := s.resource(entity.URL, resName, revision)
 	if err != nil {
 		return err
 	}

internal/charmstore/resources_test.go
@@ -5,7 +5,9 @@ package charmstore // import "gopkg.in/juju/charmstore.v5-unstable/internal/char
 
 import (
 	"bytes"
+	"io"
 	"io/ioutil"
+	"os"
 	"path/filepath"
 
 	jc "github.com/juju/testing/checkers"
@@ -87,14 +89,36 @@ func (s *ResourcesSuite) TestListResourcesResourceNotFound(c *gc.C) {
 	c.Assert(err, jc.ErrorIsNil)
 	entity, err := store.FindEntity(resolvedURL, nil)
 	c.Assert(err, jc.ErrorIsNil)
-	expected[1].Revision = addResource(c, store, entity, expected[1])
-	expected[2].Revision = addResource(c, store, entity, expected[2])
+	expected[1].Revision = addResource(c, store, entity, expected[1], nil)
+	expected[2].Revision = addResource(c, store, entity, expected[2], nil)
 
 	resources, err := store.ListResources(entity)
 	c.Assert(err, jc.ErrorIsNil)
 
 	resource.Sort(resources)
 	c.Check(resources, jc.DeepEquals, expected)
+}
+
+func (s *ResourcesSuite) TestOpenResource(c *gc.C) {
+	store := s.newStore(c, false)
+	defer store.Close()
+	curl := charm.MustParseURL("cs:~charmers/xenial/starsay-3")
+	resolvedURL := MustParseResolvedURL(curl.String())
+	_, allExpected := addCharmWithResources(c, store, curl)
+	resource.Sort(allExpected)
+	expected := allExpected[1]
+	ch := storetesting.Charms.CharmDir(curl.Name)
+	meta := ch.Meta().Resources["for-store"]
+	expectedData, err := ioutil.ReadFile(filepath.Join(ch.Path, meta.Path))
+
+	res, reader, err := store.OpenResource(resolvedURL, meta.Name, -1)
+	c.Assert(err, jc.ErrorIsNil)
+	defer reader.Close()
+
+	c.Check(res, jc.DeepEquals, expected)
+	data, err := ioutil.ReadAll(reader)
+	c.Assert(err, jc.ErrorIsNil)
+	c.Check(data, jc.DeepEquals, expectedData)
 }
 
 func addCharmWithResources(c *gc.C, store *Store, curl *charm.URL) (*mongodoc.Entity, []resource.Resource) {
@@ -108,15 +132,23 @@ func addCharmWithResources(c *gc.C, store *Store, curl *charm.URL) (*mongodoc.En
 
 	resources := extractResources(c, ch)
 	for i, res := range resources {
-		resources[i].Revision = addResource(c, store, entity, res)
+		blob, err := os.Open(filepath.Join(ch.Path, res.Path))
+		c.Assert(err, jc.ErrorIsNil)
+		resources[i].Revision = addResource(c, store, entity, res, blob)
 	}
 	return entity, resources
 }
 
-func addResource(c *gc.C, store *Store, entity *mongodoc.Entity, res resource.Resource) int {
+func addResource(c *gc.C, store *Store, entity *mongodoc.Entity, res resource.Resource, blob io.Reader) int {
 	revision := res.Revision + 1
-	err := store.insertResource(entity, res, revision)
-	c.Assert(err, jc.ErrorIsNil)
+	var err error
+	if blob != nil {
+		err := store.addResource(entity, res, blob, revision)
+		c.Assert(err, jc.ErrorIsNil)
+	} else {
+		err := store.insertResource(entity, res, "", revision)
+		c.Assert(err, jc.ErrorIsNil)
+	}
 	err = store.setResource(entity, res.Name, revision)
 	c.Assert(err, jc.ErrorIsNil)
 	return revision

juju/charmstore

internal/charmstore/resources.go
@@ -4,16 +4,19 @@
 package charmstore
 
 import (
+	"fmt"
 	"io"
 	"time"
 
 	"gopkg.in/errgo.v1"
 	"gopkg.in/juju/charm.v6-unstable"
+	"gopkg.in/juju/charm.v6-unstable/resource"
 	"gopkg.in/juju/charmrepo.v2-unstable/csclient/params"
 	"gopkg.in/mgo.v2"
 	"gopkg.in/mgo.v2/bson"
 
 	"gopkg.in/juju/charmstore.v5-unstable/internal/mongodoc"
+	"gopkg.in/juju/charmstore.v5-unstable/internal/router"
 )
 
 var resourceNotFound = errgo.Newf("resource not found")
@@ -50,6 +53,51 @@ func (s Store) ListResources(entity *mongodoc.Entity, channel params.Channel) ([
 	}
 	mongodoc.SortResources(docs)
 	return docs, nil
+}
+
+// OpenResource returns the blob for the identified resource.
+func (s *Store) OpenResource(id *router.ResolvedURL, name string, revision int) (*mongodoc.Resource, io.ReadCloser, error) {
+	if revision < 0 {
+		return nil, nil, errgo.New("revision cannot be negative")
+	}
+	doc, err := s.resource(&id.URL, name, revision)
+	if err != nil {
+		return nil, nil, errgo.Mask(err, errgo.Is(resourceNotFound))
+	}
+	r, err := s.openResource(doc)
+	if err != nil {
+		return nil, nil, errgo.Mask(err)
+	}
+	return doc, r, nil
+}
+
+// OpenResource returns the blob for the latest revision of the identified resource.
+func (s *Store) OpenLatestResource(id *router.ResolvedURL, channel params.Channel, name string) (*mongodoc.Resource, io.ReadCloser, error) {
+	entity, err := s.FindEntity(id, nil)
+	if err != nil {
+		return nil, nil, errgo.Mask(err, errgo.Is(params.ErrNotFound))
+	}
+	revision, err := s.latestResourceRevision(entity, channel, name)
+	if err != nil {
+		return nil, nil, errgo.Mask(err, errgo.Is(resourceNotFound))
+	}
+	doc, reader, err := s.OpenResource(id, name, revision)
+	if err != nil {
+		return nil, nil, errgo.Mask(err, errgo.Is(resourceNotFound))
+	}
+	return doc, reader, nil
+}
+
+func (s *Store) openResource(doc *mongodoc.Resource) (io.ReadCloser, error) {
+	r, size, err := s.BlobStore.Open(doc.BlobName)
+	if err != nil {
+		return nil, errgo.Notef(err, "cannot open resource data for %s", doc.Name)
+	}
+	if size != doc.Size {
+		return nil, errgo.Newf("resource size mismatch")
+	}
+	// TODO(ericsnow) Verify that the hash matches?
+	return r, nil
 }
 
 // ResourceBlob holds the information specific to a single resource blob.
@@ -109,9 +157,29 @@ func (s Store) insertResource(doc *mongodoc.Resource) error {
 }
 
 func (s Store) storeResource(blob ResourceBlob) (string, error) {
-	name := bson.NewObjectId().Hex()
-	// TODO(ericsnow) We will finish this in a follow-up patch.
-	return name, nil
+	blobName := bson.NewObjectId().Hex()
+
+	// Calculate the SHA384 hash while uploading the blob in the blob store.
+	fpHash := resource.NewFingerprintHash()
+	blobReader := io.TeeReader(blob, fpHash)
+
+	// Upload the actual blob, and make sure that it is removed
+	// if we fail later.
+	hash := fmt.Sprintf("%x", blob.Fingerprint)
+	err := s.BlobStore.PutUnchallenged(blobReader, blobName, blob.Size, hash)
+	if err != nil {
+		return "", errgo.Notef(err, "cannot put archive blob")
+	}
+
+	fp := fpHash.Fingerprint()
+	if fp.String() != hash {
+		if err := s.BlobStore.Remove(blobName); err != nil {
+			logger.Errorf("cannot remove blob %s after error: %v", blobName, err)
+		}
+		return "", errgo.Newf("resource hash mismatch")
+	}
+
+	return blobName, nil
 }
 
 func (s Store) setResource(entity *mongodoc.Entity, channel params.Channel, resName string, revision int) error {

internal/charmstore/resources_test.go
@@ -88,6 +88,53 @@ func (s *ResourcesSuite) TestListResourcesResourceNotFound(c *gc.C) {
 	checkResourceDocs(c, docs, expected)
 }
 
+func (s *ResourcesSuite) TestOpenResource(c *gc.C) {
+	store := s.newStore(c, false)
+	defer store.Close()
+	curl := charm.MustParseURL("cs:~charmers/xenial/starsay-3")
+	resolvedURL := MustParseResolvedURL(curl.String())
+	entity, ch := addCharm(c, store, curl)
+	allExpected := addResources(c, store, curl, params.UnpublishedChannel, entity, ch)
+	mongodoc.SortResources(allExpected)
+	expected := allExpected[1]
+	meta := ch.Meta().Resources["for-store"]
+	expectedData, err := ioutil.ReadFile(filepath.Join(ch.Path, meta.Path))
+
+	doc, reader, err := store.OpenResource(resolvedURL, meta.Name, 1)
+	c.Assert(err, jc.ErrorIsNil)
+	defer reader.Close()
+
+	adjustExpectedResource(doc, expected)
+	c.Check(doc, jc.DeepEquals, expected)
+	data, err := ioutil.ReadAll(reader)
+	c.Assert(err, jc.ErrorIsNil)
+	c.Check(data, jc.DeepEquals, expectedData)
+}
+
+func (s *ResourcesSuite) TestOpenLatestResource(c *gc.C) {
+	store := s.newStore(c, false)
+	defer store.Close()
+	channel := params.StableChannel
+	curl := charm.MustParseURL("cs:~charmers/xenial/starsay-3")
+	resolvedURL := MustParseResolvedURL(curl.String())
+	entity, ch := addCharm(c, store, curl)
+	allExpected := addResources(c, store, curl, channel, entity, ch)
+	mongodoc.SortResources(allExpected)
+	expected := allExpected[1]
+	meta := ch.Meta().Resources["for-store"]
+	expectedData, err := ioutil.ReadFile(filepath.Join(ch.Path, meta.Path))
+
+	doc, reader, err := store.OpenLatestResource(resolvedURL, channel, meta.Name)
+	c.Assert(err, jc.ErrorIsNil)
+	defer reader.Close()
+
+	adjustExpectedResource(doc, expected)
+	c.Check(doc, jc.DeepEquals, expected)
+	data, err := ioutil.ReadAll(reader)
+	c.Assert(err, jc.ErrorIsNil)
+	c.Check(data, jc.DeepEquals, expectedData)
+}
+
 func addResources(c *gc.C, store *Store, curl *charm.URL, channel params.Channel, entity *mongodoc.Entity, ch *charm.CharmDir) []*mongodoc.Resource {
 	docs := extractResources(c, curl, ch)
 	for i, doc := range docs {