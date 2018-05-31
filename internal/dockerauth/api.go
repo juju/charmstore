8974ae9c417c47ccdb917a476d350d0d243823c8
Martin Hilton <martin.hilton@canonical.com>
2018-05-31 13:33:07 +0100

    internal/dockerauth: use macaroon for authentication

--- internal/dockerauth/api.go
+++ internal/dockerauth/api.go
@@ -4,10 +4,8 @@
 package dockerauth
 
 import (
-	"crypto"
 	"crypto/ecdsa"
 	"crypto/rsa"
-	"crypto/x509"
 	"encoding/base64"
 	"fmt"
 	"net/http"
@@ -15,12 +13,19 @@ import (
 	"time"
 
 	jwt "github.com/dgrijalva/jwt-go"
+	"github.com/juju/idmclient"
 	"github.com/juju/loggo"
 	"github.com/julienschmidt/httprouter"
 	"golang.org/x/net/context"
 	errgo "gopkg.in/errgo.v1"
 	httprequest "gopkg.in/httprequest.v1"
+	charm "gopkg.in/juju/charm.v6"
+	"gopkg.in/juju/charmrepo.v3/csclient/params"
+	"gopkg.in/macaroon-bakery.v2-unstable/bakery/checkers"
+	"gopkg.in/macaroon.v2-unstable"
+
 	"gopkg.in/juju/charmstore.v5/internal/charmstore"
+	"gopkg.in/juju/charmstore.v5/internal/mongodoc"
 )
 
 var logger = loggo.GetLogger("charmstore.internal.dockerauth")
@@ -29,8 +34,8 @@ var logger = loggo.GetLogger("charmstore.internal.dockerauth")
 // from a scope. This is parsed as a resourcescope from the grammer
 // specified in
 // https://docs.docker.com/registry/spec/auth/scope/#resource-scope-grammar.
-func parseResourceAccessRights(s string) (ResourceAccessRights, error) {
-	var ra ResourceAccessRights
+func parseResourceAccessRights(s string) (resourceAccessRights, error) {
+	var ra resourceAccessRights
 	i := strings.IndexByte(s, ':')
 	j := strings.LastIndexByte(s, ':')
 	if i == j {
@@ -45,16 +50,16 @@ func parseResourceAccessRights(s string) (ResourceAccessRights, error) {
 	return ra, nil
 }
 
-// ParseScope parses a requested scope and returns the set or requested
+// parseScope parses a requested scope and returns the set or requested
 // resource accesses. An error of type *ScopeParseError is returned if
 // any part of the scope is not valid. Any valid resource scopes are
 // always returned.
-func ParseScope(s string) ([]ResourceAccessRights, error) {
+func parseScope(s string) ([]resourceAccessRights, error) {
 	if s == "" {
 		return nil, nil
 	}
 	var errs []error
-	var ras []ResourceAccessRights
+	var ras []resourceAccessRights
 	for _, s := range strings.Split(s, " ") {
 		ra, err := parseResourceAccessRights(s)
 		if err != nil {
@@ -64,32 +69,31 @@ func ParseScope(s string) ([]ResourceAccessRights, error) {
 		ras = append(ras, ra)
 	}
 	if len(errs) > 0 {
-		return ras, &ScopeParseError{errs}
+		return ras, &scopeParseError{errs}
 	}
 	return ras, nil
 }
 
-// A ScopeParseError is an error returned when part of a scope is not
+// A scopeParseError is an error returned when part of a scope is not
 // valid.
-type ScopeParseError struct {
-	Errs []error
+type scopeParseError struct {
+	errs []error
 }
 
 // Error implements error.
-func (e *ScopeParseError) Error() string {
-	if len(e.Errs) == 1 {
-		return e.Errs[0].Error()
+func (e *scopeParseError) Error() string {
+	if len(e.errs) == 1 {
+		return e.errs[0].Error()
 	}
-	errs := make([]string, len(e.Errs))
-	for i, err := range e.Errs {
+	errs := make([]string, len(e.errs))
+	for i, err := range e.errs {
 		errs[i] = err.Error()
 	}
 	return fmt.Sprintf("[%s]", strings.Join(errs, ", "))
 }
 
 type Handler struct {
-	Key        crypto.Signer
-	Certs      []*x509.Certificate
+	params     charmstore.APIHandlerParams
 	TokenValid time.Duration
 }
 
@@ -107,23 +111,151 @@ type tokenResponse struct {
 
 type dockerRegistryClaims struct {
 	jwt.StandardClaims
-	Access []ResourceAccessRights `json:"access"`
+	Access []resourceAccessRights `json:"access"`
 }
 
-func (h *Handler) Token(p httprequest.Params, req *tokenRequest) (*tokenResponse, error) {
-	ras, err := ParseScope(req.Scope)
+func (h *Handler) handler(p httprequest.Params) (*handler, context.Context, error) {
+	store, err := h.params.Pool.RequestStore()
 	if err != nil {
-		logger.Infof("Invalid scope: %s", err)
+		return nil, p.Context, errgo.Mask(err)
 	}
-	var issuer string
-	if len(h.Certs) > 0 {
-		issuer = h.Certs[0].Subject.CommonName
+	return &handler{h, store}, p.Context, nil
+}
+
+type handler struct {
+	h     *Handler
+	store *charmstore.Store
+}
+
+func (h *handler) Close() error {
+	h.store.Close()
+	return nil
+}
+
+// Token implements the token issuing endpoint for a docker registry
+// authorization service. See
+// https://docs.docker.com/registry/spec/auth/token/ for the protocol
+// details.
+func (h *handler) Token(p httprequest.Params, req *tokenRequest) (*tokenResponse, error) {
+	ras, err := parseScope(req.Scope)
+	if err != nil {
+		return nil, errgo.Mask(err)
+	}
+	ms := credentials(p.Request)
+	declared := checkers.InferDeclared(ms)
+	// TODO(mhilton) support the "everyone" ACL entry to allow
+	// unauthenticated access.
+	var user user = noUser{}
+	if identity, err := h.h.params.IDMClient.DeclaredIdentity(declared); err == nil {
+		if u, ok := identity.(*idmclient.User); ok {
+			user = u
+		}
+	}
+	filteredRAs := make([]resourceAccessRights, 0, len(ras))
+	for _, ra := range ras {
+		if ra.Type != "repository" {
+			continue
+		}
+		acl, err := h.repositoryACL(ra.Name)
+		if err != nil {
+			return nil, errgo.Mask(err)
+		}
+		filteredActions := make([]string, 0, len(ra.Actions))
+		for _, a := range ra.Actions {
+			switch a {
+			case "pull":
+				allow, _ := user.Allow(acl.Read)
+				if !allow {
+					continue
+				}
+			case "push":
+				allow, _ := user.Allow(acl.Write)
+				if !allow {
+					continue
+				}
+			default:
+				continue
+			}
+			// TODO(mhilton) support more caveats.
+			if h.store.Bakery.Check(ms, checkers.New(declared)) != nil {
+				continue
+			}
+			filteredActions = append(filteredActions, a)
+		}
+		if len(filteredActions) == 0 {
+			continue
+		}
+		filteredRAs = append(filteredRAs, resourceAccessRights{
+			Type:    ra.Type,
+			Name:    ra.Name,
+			Actions: filteredActions,
+		})
 	}
 	issuedAt := time.Now()
-	expiresAt := issuedAt.Add(h.TokenValid)
+	s, err := h.createToken(filteredRAs, req.Service, issuedAt)
+	if err != nil {
+		return nil, errgo.Mask(err)
+	}
+	return &tokenResponse{
+		Token:     s,
+		ExpiresIn: int(h.h.TokenValid / time.Second),
+		IssuedAt:  issuedAt,
+	}, nil
+}
+
+func credentials(req *http.Request) macaroon.Slice {
+	_, pw, _ := req.BasicAuth()
+	b, err := base64.RawStdEncoding.DecodeString(pw)
+	if err != nil {
+		logger.Debugf("invalid macaroon: %s", err)
+		return nil
+	}
+	var ms macaroon.Slice
+	if err := ms.UnmarshalBinary(b); err != nil {
+		logger.Debugf("invalid macaroon: %s", err)
+		return nil
+	}
+	return ms
+}
+
+// repositoryACL finds the ACL corresponding to the given
+// docker-repository name. A repository name is of the form
+// <owner>/<charm name>/<channel>/<resource name>. If an invalid name is
+// given then an empty ACL will be returned causing the access request to
+// be denied.
+func (h *handler) repositoryACL(name string) (mongodoc.ACL, error) {
+	parts := strings.SplitN(name, "/", 4)
+	if len(parts) != 4 {
+		return mongodoc.ACL{}, nil
+	}
+	baseURL := charm.URL{
+		Schema:   "cs",
+		User:     parts[0],
+		Name:     parts[1],
+		Revision: -1,
+	}
+	be, err := h.store.FindBaseEntity(&baseURL, map[string]int{
+		"_id":         1,
+		"channelacls": 1,
+	})
+	if err != nil {
+		return mongodoc.ACL{}, errgo.Mask(err)
+	}
+	// TODO(mhilton) validate the resource name corresponds to a charm resource?
+	return be.ChannelACLs[params.Channel(parts[2])], nil
+}
+
+// createToken creates a JWT for the given service with the givne access
+// rights.
+func (h *handler) createToken(ras []resourceAccessRights, service string, issuedAt time.Time) (string, error) {
+	var issuer string
+	if len(h.h.params.DockerRegistryAuthCertificates) > 0 {
+		issuer = h.h.params.DockerRegistryAuthCertificates[0].Subject.CommonName
+	}
+	expiresAt := issuedAt.Add(h.h.TokenValid)
 	claims := dockerRegistryClaims{
 		StandardClaims: jwt.StandardClaims{
-			Audience:  req.Service,
+			Audience:  service,
 			ExpiresAt: expiresAt.Unix(),
 			IssuedAt:  issuedAt.Unix(),
 			NotBefore: issuedAt.Unix(),
@@ -132,7 +264,7 @@ func (h *Handler) Token(p httprequest.Params, req *tokenRequest) (*tokenResponse
 		Access: ras,
 	}
 	var sm jwt.SigningMethod
-	switch h.Key.(type) {
+	switch h.h.params.DockerRegistryAuthKey.(type) {
 	case *ecdsa.PrivateKey:
 		sm = jwt.SigningMethodES256
 	case *rsa.PrivateKey:
@@ -141,42 +273,32 @@ func (h *Handler) Token(p httprequest.Params, req *tokenRequest) (*tokenResponse
 		sm = jwt.SigningMethodNone
 	}
 	tok := jwt.NewWithClaims(sm, claims)
-	certs := make([]string, len(h.Certs))
-	for i, c := range h.Certs {
+	certs := make([]string, len(h.h.params.DockerRegistryAuthCertificates))
+	for i, c := range h.h.params.DockerRegistryAuthCertificates {
 		certs[i] = base64.StdEncoding.EncodeToString(c.Raw)
 	}
 	// The x5c header contains the certificate chain used by the
 	// docker-registry to authenticate the token.
 	tok.Header["x5c"] = certs
-	s, err := tok.SignedString(h.Key)
+	s, err := tok.SignedString(h.h.params.DockerRegistryAuthKey)
 	if err != nil {
-		return nil, errgo.Mask(err)
+		return "", errgo.Mask(err)
 	}
-	return &tokenResponse{
-		Token:     s,
-		ExpiresIn: int(h.TokenValid / time.Second),
-		IssuedAt:  issuedAt,
-	}, nil
-}
-
-func (h *Handler) handler(p httprequest.Params) (*Handler, context.Context, error) {
-	return h, p.Context, nil
+	return s, nil
 }
 
 func NewAPIHandler(p charmstore.APIHandlerParams) (charmstore.HTTPCloseHandler, error) {
 	logger.Infof("Adding docker-registry")
 	h := &Handler{
-		Key:        p.DockerRegistryAuthKey,
-		Certs:      p.DockerRegistryAuthCertificates,
+		params: p,
+		// TODO(mhilton) make TokenValid configurable.
 		TokenValid: 120 * time.Second,
 	}
 	r := httprouter.New()
 	srv := httprequest.Server{
 		ErrorMapper: errorMapper,
 	}
-	httprequest.AddHandlers(r, srv.Handlers(func(p httprequest.Params) (*Handler, context.Context, error) {
-		return h, p.Context, nil
-	}))
+	httprequest.AddHandlers(r, srv.Handlers(h.handler))
 	return server{
 		Handler: r,
 	}, nil
@@ -189,7 +311,20 @@ type server struct {
 func (server) Close() {}
 
 func errorMapper(ctx context.Context, err error) (httpStatus int, errorBody interface{}) {
+	// TODO return docker-registry standard error format (see
+	// https://docs.docker.com/registry/spec/api/#errors)
 	return http.StatusInternalServerError, &httprequest.RemoteError{
 		Message: err.Error(),
 	}
+}
+
+type user interface {
+	Allow([]string) (bool, error)
+}
+
+type noUser struct {
+}
+
+func (noUser) Allow([]string) (bool, error) {
+	return false, nil
 }
--- internal/dockerauth/api_test.go
+++ internal/dockerauth/api_test.go
@@ -10,112 +10,34 @@ import (
 	"crypto/rand"
 	"crypto/x509"
 	"crypto/x509/pkix"
+	"encoding/base64"
 	"math/big"
+	"net/http"
 	"net/http/httptest"
 
 	jwt "github.com/dgrijalva/jwt-go"
+	"github.com/juju/idmclient"
+	"github.com/juju/idmclient/idmtest"
+	jujutesting "github.com/juju/testing"
 	jc "github.com/juju/testing/checkers"
 	"golang.org/x/net/context"
 	gc "gopkg.in/check.v1"
 	httprequest "gopkg.in/httprequest.v1"
+	charm "gopkg.in/juju/charm.v6"
+	"gopkg.in/juju/charmrepo.v3/csclient/params"
+	"gopkg.in/macaroon-bakery.v2-unstable/bakery/checkers"
+	macaroon "gopkg.in/macaroon.v2-unstable"
 
 	"gopkg.in/juju/charmstore.v5/internal/charmstore"
 	"gopkg.in/juju/charmstore.v5/internal/dockerauth"
+	"gopkg.in/juju/charmstore.v5/internal/mongodoc"
 )
 
-type APISuite struct{}
+type APISuite struct {
+	jujutesting.IsolatedMgoSuite
+}
 
 var _ = gc.Suite(&APISuite{})
-
-var parseScopeTests = []struct {
-	scope                      string
-	expectResourceAccessRights []dockerauth.ResourceAccessRights
-	expectError                string
-}{{
-	scope: "",
-}, {
-	scope:       "bad",
-	expectError: `invalid resource scope "bad"`,
-}, {
-	scope: "repository:1/2/3/4:push,pull",
-	expectResourceAccessRights: []dockerauth.ResourceAccessRights{
-		{
-			Type:    "repository",
-			Name:    "1/2/3/4",
-			Actions: []string{"push", "pull"},
-		},
-	},
-}, {
-	scope: "repository:1/2/3/4:push,pull bad repository:5/6/7/8: more:bad repository:9/10:push",
-	expectResourceAccessRights: []dockerauth.ResourceAccessRights{
-		{
-			Type:    "repository",
-			Name:    "1/2/3/4",
-			Actions: []string{"push", "pull"},
-		},
-		{
-			Type: "repository",
-			Name: "5/6/7/8",
-		},
-		{
-			Type:    "repository",
-			Name:    "9/10",
-			Actions: []string{"push"},
-		},
-	},
-	expectError: `\[invalid resource scope "bad", invalid resource scope "more:bad"\]`,
-}}
-
-func (s *APISuite) TestParseScope(c *gc.C) {
-	for i, test := range parseScopeTests {
-		c.Logf("%d. %q", i, test.scope)
-		ras, err := dockerauth.ParseScope(test.scope)
-		c.Assert(ras, jc.DeepEquals, test.expectResourceAccessRights)
-		if test.expectError == "" {
-			c.Assert(err, gc.Equals, nil)
-		} else {
-			c.Assert(err, gc.ErrorMatches, test.expectError)
-		}
-	}
-}
-
-func (s *APISuite) TestToken(c *gc.C) {
-	cert, key := newCert(c)
-
-	hnd, err := dockerauth.NewAPIHandler(charmstore.APIHandlerParams{
-		ServerParams: charmstore.ServerParams{
-			DockerRegistryAuthKey: key,
-			DockerRegistryAuthCertificates: []*x509.Certificate{
-				cert,
-			},
-		},
-	})
-	c.Assert(err, gc.Equals, nil)
-	srv := httptest.NewServer(hnd)
-	defer srv.Close()
-
-	client := httprequest.Client{
-		BaseURL: srv.URL,
-	}
-	var resp dockerauth.TokenResponse
-	err = client.Get(context.Background(), "/token?service=myregistry&scope=repository:myrepo:pull", &resp)
-	c.Assert(err, gc.Equals, nil)
-	tok, err := jwt.Parse(resp.Token, func(_ *jwt.Token) (interface{}, error) {
-		return key.Public(), nil
-	})
-	c.Assert(err, gc.Equals, nil)
-	claims, ok := tok.Claims.(jwt.MapClaims)
-	c.Assert(ok, gc.Equals, true)
-	c.Assert(claims["access"], jc.DeepEquals, []interface{}{
-		map[string]interface{}{
-			"type": "repository",
-			"name": "myrepo",
-			"actions": []interface{}{
-				"pull",
-			},
-		},
-	})
-}
 
 func newCert(c *gc.C) (_ *x509.Certificate, key crypto.Signer) {
 	key, err := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
@@ -126,9 +48,157 @@ func newCert(c *gc.C) (_ *x509.Certificate, key crypto.Signer) {
 			CommonName: "test",
 		},
 	}
-	raw, err := x509.CreateCertificate(rand.Reader, &template, &template, key.(crypto.Signer).Public(), key)
+	raw, err := x509.CreateCertificate(rand.Reader, &template, &template, key.Public(), key)
 	c.Assert(err, gc.Equals, nil)
 	cert, err := x509.ParseCertificate(raw)
 	c.Assert(err, gc.Equals, nil)
 	return cert, key
+}
+
+func (s *APISuite) newServer(c *gc.C, cert *x509.Certificate, key crypto.Signer) (*charmstore.Server, *charmstore.Store, *idmtest.Server) {
+	idmServer := idmtest.NewServer()
+	idmServer.AddUser("charmstore-agent")
+	db := s.Session.DB("docker-registry-test")
+	srv, err := charmstore.NewServer(db, nil, charmstore.ServerParams{
+		AgentUsername:         "charmstore-agent",
+		AgentKey:              idmServer.UserPublicKey("charmstore-agent"),
+		DockerRegistryAuthKey: key,
+		DockerRegistryAuthCertificates: []*x509.Certificate{
+			cert,
+		},
+		IdentityLocation: idmServer.URL.String(),
+	}, map[string]charmstore.NewAPIHandlerFunc{
+		"docker-registry": dockerauth.NewAPIHandler,
+	})
+	c.Assert(err, gc.Equals, nil)
+	return srv, srv.Pool().Store(), idmServer
+}
+
+func (s *APISuite) TestValidMacaroonToken(c *gc.C) {
+	cert, key := newCert(c)
+	hnd, store, idmServer := s.newServer(c, cert, key)
+	defer hnd.Close()
+	defer store.Close()
+	defer idmServer.Close()
+	srv := httptest.NewServer(hnd)
+	defer srv.Close()
+
+	err := store.DB.BaseEntities().Insert(&mongodoc.BaseEntity{
+		URL: &charm.URL{
+			Schema:   "cs",
+			User:     "bob",
+			Name:     "test",
+			Revision: -1,
+		},
+		ChannelACLs: map[params.Channel]mongodoc.ACL{
+			params.StableChannel: {
+				Read:  []string{"alice", "bob"},
+				Write: []string{"bob"},
+			},
+		},
+	})
+	c.Assert(err, gc.Equals, nil)
+
+	m, err := store.Bakery.NewMacaroon([]checkers.Caveat{
+		idmclient.UserDeclaration("alice"),
+	})
+	c.Assert(err, gc.Equals, nil)
+
+	ms := macaroon.Slice{m}
+	b, err := ms.MarshalBinary()
+	c.Assert(err, gc.Equals, nil)
+
+	req, err := http.NewRequest("GET", "/docker-registry/token?service=myregistry&scope=repository:bob/test/stable/test-resource:pull,push", nil)
+	c.Assert(err, gc.Equals, nil)
+	req.SetBasicAuth("alice", base64.RawStdEncoding.EncodeToString(b))
+
+	client := httprequest.Client{
+		BaseURL: srv.URL,
+	}
+	var resp dockerauth.TokenResponse
+	err = client.Do(context.Background(), req, &resp)
+	c.Assert(err, gc.Equals, nil)
+	tok, err := jwt.Parse(resp.Token, func(_ *jwt.Token) (interface{}, error) {
+		return key.Public(), nil
+	})
+	c.Assert(err, gc.Equals, nil)
+	claims, ok := tok.Claims.(jwt.MapClaims)
+	c.Assert(ok, gc.Equals, true)
+	c.Assert(claims["access"], jc.DeepEquals, []interface{}{map[string]interface{}{
+		"type": "repository",
+		"name": "bob/test/stable/test-resource",
+		"actions": []interface{}{
+			"pull",
+		},
+	}})
+}
+
+func (s *APISuite) TestUnauthenticatedToken(c *gc.C) {
+	cert, key := newCert(c)
+	hnd, store, idmServer := s.newServer(c, cert, key)
+	defer hnd.Close()
+	defer store.Close()
+	defer idmServer.Close()
+	srv := httptest.NewServer(hnd)
+	defer srv.Close()
+
+	client := httprequest.Client{
+		BaseURL: srv.URL,
+	}
+	var resp dockerauth.TokenResponse
+	err := client.Get(context.Background(), "/docker-registry/token?service=myregistry&scope=repository:myrepo:pull,push", &resp)
+	c.Assert(err, gc.Equals, nil)
+	tok, err := jwt.Parse(resp.Token, func(_ *jwt.Token) (interface{}, error) {
+		return key.Public(), nil
+	})
+	c.Assert(err, gc.Equals, nil)
+	claims, ok := tok.Claims.(jwt.MapClaims)
+	c.Assert(ok, gc.Equals, true)
+	c.Assert(claims["access"], jc.DeepEquals, []interface{}{})
+}
+
+func (s *APISuite) TestInvalidScope(c *gc.C) {
+	cert, key := newCert(c)
+	hnd, store, idmServer := s.newServer(c, cert, key)
+	defer hnd.Close()
+	defer store.Close()
+	defer idmServer.Close()
+	srv := httptest.NewServer(hnd)
+	defer srv.Close()
+
+	err := store.DB.BaseEntities().Insert(&mongodoc.BaseEntity{
+		URL: &charm.URL{
+			Schema:   "cs",
+			User:     "bob",
+			Name:     "test",
+			Revision: -1,
+		},
+		ChannelACLs: map[params.Channel]mongodoc.ACL{
+			params.StableChannel: {
+				Read:  []string{"alice", "bob"},
+				Write: []string{"bob"},
+			},
+		},
+	})
+	c.Assert(err, gc.Equals, nil)
+
+	m, err := store.Bakery.NewMacaroon([]checkers.Caveat{
+		idmclient.UserDeclaration("alice"),
+	})
+	c.Assert(err, gc.Equals, nil)
+
+	ms := macaroon.Slice{m}
+	b, err := ms.MarshalBinary()
+	c.Assert(err, gc.Equals, nil)
+
+	req, err := http.NewRequest("GET", "/docker-registry/token?service=myregistry&scope=repository:pull,push", nil)
+	c.Assert(err, gc.Equals, nil)
+	req.SetBasicAuth("alice", base64.RawStdEncoding.EncodeToString(b))
+
+	client := httprequest.Client{
+		BaseURL: srv.URL,
+	}
+	var resp dockerauth.TokenResponse
+	err = client.Do(context.Background(), req, &resp)
+	c.Assert(err, gc.ErrorMatches, `Get .*/docker-registry/token\?service=myregistry&scope=repository:pull,push: invalid resource scope "repository:pull,push"`)
 }
